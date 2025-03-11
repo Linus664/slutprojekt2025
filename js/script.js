fa53e9ed09ef3a27a6a7bfc4129f3edb034699ca
Linus664 <linus.moller@elev.ga.ntig.se>
2025-03-11 20:14:37 +0100

    Laddar korten/spelen med javaskript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
-//JAVASCRIPT FIL HÄR
+document.addEventListener("DOMContentLoaded", () => {
+  let games = [
+    {
+      title: "Sonic Colors",
+      image: "games/Sonic-Colors.jpg",
+    },
+    {
+      title: "Mario Kart 8",
+      image: "games/Mario-Kart-8.jpg",
+    },
+    {
+      title: "Pokemon Brilliant Diamond",
+      image: "games/Pokemon-Brilliant-Diamond.jpg",
+    },
+    {
+      title: "Pokemon Shining Pearl",
+      image: "games/Pokemon-Shining-Pearl.jpg",
+    },
+    {
+      title: "Pokemon Let's Go Pikachu",
+      image: "games/Pokemon-Let's-Go-Pikachu.jpg",
+    },
+    {
+      title: "Pokemon Sword",
+      image: "games/Pokemon-Sword.jpg",
+    },
+    {
+      title: "Super Smash Bros",
+      image: "games/Super-Smash-Bros.jpg",
+    },
+    {
+      title: "Zelda",
+      image: "games/Zelda.jpg",
+    },
+    {
+      title: "Metroid",
+      image: "games/Metroid.jpg",
+    },
+  ];
+  const container = document.getElementById("cardContainer");
+
+  games.forEach((game) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <img src="${game.image}" class="game-preview">
+      <p class="game-title">${game.title}</p>
+      <button class="borrow-button">Borrow</button>
+    `;
+
+    container.appendChild(card);
+  });
+});
